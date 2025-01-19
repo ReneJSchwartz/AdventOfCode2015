37696b3952b7c722a6151dc583444999e75d9a18
Rene Schwartz <rene.j.schwartz@gmail.com>
2025-01-19 18:05:25 +0200

    Add part 2 to day 2

--- day_02_no_math.pm
+++ day_02_no_math.pm
@@ -14,7 +14,7 @@ my $paper_needed = 0;
 for my $line (split('\n', $gifts)) {
     (my $l, my $w, my $h) = $line =~ /\d+/g;  
     my $smallest_side = min($l * $w, $w * $h, $h * $l);
-    say "l$l w$w h$h and smallest is $smallest_side";
+    # say "l$l w$w h$h and smallest is $smallest_side";
     # 2*l*w + 2*w*h + 2*h*l + smallest side
     $paper_needed += 2 * $l * $w + 2 * $w * $h + 2 * $h * $l + $smallest_side;
 }
@@ -24,5 +24,16 @@ say "Part 1 wrapping paper needed: $paper_needed square feet.";
 
 # Day 2: I Was Told There Would Be No Math
 # Part 2
-# 
+# Counts how much ribbon is needed:
+# smallest perimeter + volume (cubic feet)
 
+my $ribbon_needed = 0;
+
+for my $line (split('\n', $gifts)) {
+    (my $l, my $w, my $h) = $line =~ /\d+/g;  
+    my $smallest_perimeter = min(2* $l + 2* $w, 2 * $w + 2* $h, 2* $h + 2* $l);
+    # say "smallest is $smallest_perimeter";
+    $ribbon_needed += $l * $w * $h + $smallest_perimeter;
+}
+
+say "Part 2 ribbon needed: $ribbon_needed.";
